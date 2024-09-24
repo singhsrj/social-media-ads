937d1ef7377914d359dc94fa820dc3e99cb37e38
Suraj Singh <165886618+singhsrj@users.noreply.github.com>
2024-09-24 09:55:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 
 # Load dataset
-dataset = pd.read_csv('/workspaces/social-media-ads/Social_Network_Ads.csv')
+dataset = pd.read_csv('Social_Network_Ads.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
